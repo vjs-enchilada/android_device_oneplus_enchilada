5c7497ad05ba05ad438fb429fa6a000bb4a7a12a
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-10-06 17:16:51 +0000

    enchilada: Switch to FlashKernel
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ DEVICE_PATH := device/oneplus/enchilada
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := enchilada_defconfig
+TARGET_KERNEL_CONFIG := flash_defconfig
 
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
